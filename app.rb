640d9afeaeb4379887e2f094bf599a8492244d0d
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-29 03:04:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  #Write a get method in the app.rb file to respond to the root URL '/'.
   
   get '/' do
     "Hello, World!"
